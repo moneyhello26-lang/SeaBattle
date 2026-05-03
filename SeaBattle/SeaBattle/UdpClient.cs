7473b3e43c7342b137ce965b14c6f34f36ca1c7a
moneyhello26-lang <moneyhello26@gmail.com>
2026-05-03 20:54:14 +0600

    UDPClient

--- SeaBattle/SeaBattle/UdpClient.cs
+++ SeaBattle/SeaBattle/UdpClient.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace SeaBattle
 {
     internal class UdpClient
     {
+        private Socket _sock;
+        private EndPoint _endPoint;
+        public event Action<string[]> OnMessageReceived;
+
+        public void Connect(string ip, int port)
+        {
+            _endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            _sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _sock.Connect(_endPoint);
+            ThreadPool.QueueUserWorkItem(_ => ReceiveLoop());
+        }
+
+        private void ReceiveLoop()
+        {
+            byte[] buf = new byte[1024];
+            EndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                int n = _sock.ReceiveFrom(buf, ref ep);
+                var parts = Encoding.UTF8.GetString(buf, 0, n).Split(';');
+                OnMessageReceived?.Invoke(parts);
+            }
+        }
+
     }
 }
