2db7015c37d839f1a70e1a1903b1f1891117718c
Adilbek645 <baber.channel.3@gmail.com>
2026-05-03 20:18:27 +0500

    GameProtocol, GameState

--- SeaBattle/SeaBattle/GameProtocol.cs
+++ SeaBattle/SeaBattle/GameProtocol.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+
+namespace SeaBattle
 {
-	public Class1()
-	{
-	}
+    internal class GameProtocol
+    {
+        public const string READY = "ready";
+        public const string SHOT = "shot";
+        public const string RESULT = "result";
+        public const string WIN = "win";
+        
+
+        public const int GRID_SIZE = 10;
+        public const int PORT = 12345;
+    }
 }
--- SeaBattle/SeaBattle/GameState.cs
+++ SeaBattle/SeaBattle/GameState.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace SeaBattle
 {
+    public enum CellState
+    {
+        Empty,
+        Ship,
+        Hit,
+        Miss
+    }
+
     internal class GameState
     {
+        public CellState[,] MyField = new CellState[GameProtocol.GRID_SIZE, GameProtocol.GRID_SIZE];
+        public CellState[,] OpponentField = new CellState[GameProtocol.GRID_SIZE, GameProtocol.GRID_SIZE];
+        public bool IsMyTurn = false;
+        public bool IsGameOver = false;
+        public bool IsReady = false;
+        public string OpponentName = "Opponent";
+        public string MyName = "Player";
+
     }
 }
